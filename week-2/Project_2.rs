59b5e99fac61ce1432f10511f94c2e34f77bd290
unknown <olaoluwa.olomodosi@pau.edu.ng>
2026-09-03 13:47:38 +0100

    (my third commit)

--- week-2/Project_2.rs
+++ week-2/Project_2.rs
@@ -1,3 +1,21 @@
 fn main (){
-	let items:f64 = ([1 Toshiba 2 450000])
+	//sales from the table
+	let toshiba: f64 = 450000.00;
+	let mac: f64 = 1500000.00;
+	let hp: f64 = 750000.00;
+	let dell: f64 = 2850000.00;
+	let acer: f64 = 250000.00;
+
+	let number_of_items: f64 = 5.0; 
+	
+	//sum of all sales
+	let mut sum: f64 =0.0;
+	sum = sum + toshiba + mac + hp + dell + acer;
+
+	//average sales
+	let average = sum / number_of_items;
+
+	println!("the total sales sum is {}",sum );
+	println!("the average sale is {}",average );
+	
 }
